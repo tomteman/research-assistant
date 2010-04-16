5c7cfca0e171b663873d8122a57b8de86a7c7b47
maverick42 <maverick42@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-04-16 09:43:29 +0000

    change "import parser" to "from bibtexParser import parser"

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
-import parser
+from bibtexParser import parser
                             #local imports
 import GeneralFuncs
 #import FollowForm
