4356aa83836a7346328e3f016a1372cc8b67c415
Lea.stolo@gmail.com <Lea.stolo@gmail.com@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-05-18 21:10:21 +0000

    This file contains now only the Follow class. class DBFollow was moved to a different file. added some fields to the class, and the methods: create_follow_name_from_search_params first_follow_query first_upload convert2DBFollow

--- src/Follow.py
+++ src/Follow.py
@@ -1,40 +1,23 @@
-#QM = "\""
 # -*- coding: cp1255 -*-
-import GeneralFuncs
-from ArticleData import *
+#QM = "\""
+
+################### Lea:  TODO for next time:
+
+# Check the follow on 8081 :) http://localhost:8081/_ah/admin/datastore?kind=DBFollow
+# some problem with the pickle #search_params_str = db.StringProperty(multiline=True)
+
+
+from SearchParams import *
+from ArticleData import ArticleData
 import datetime
-from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext import db
-import os
-from google.appengine.ext.webapp import template
-
-#test
-
-DEBUG = False
-
-# TODO: check how to do enum for update_frequency, max_results_in_update etc.
+import string
+import HTMLparser
+from DBFollow import DBFollow
+import pickle
 # TODO: change defaults in the __init__ function
-
-
-
-
-# see results in http://localhost:8080/
-
 # TODO : decide which fields should be required
-class DBFollow(db.Model):
-    user = db.UserProperty()
-    follow_name = db.StringProperty()
-    search_params_str = db.TextProperty()
-    update_frequency = db.StringProperty(choices=set(["day","week","monthly"]))
-    max_results_in_update = db.IntegerProperty(choices=set([10,20,50]))
-    num_of_updates = db.IntegerProperty()
-    num_of_successful_updates = db.IntegerProperty()
-    time_first_created = db.DateTimeProperty()
-    time_last_updated = db.DateTimeProperty()
-    pastResultsKeysList_str = db.TextProperty()
-    url = db.TextProperty()
+
+
     
 class Follow:
     def __init__(self, 
@@ -43,14 +26,19 @@ class Follow:
                  user_id = None,
                  follow_name = None, 
                  search_params = None, 
-                 update_frequency = "weekly", 
+                 update_frequency = "weekly",
+                 is_empty_query = False, # this is filled with True when user added a follow 
+                                         # and was prompt that the search params he gave result in a url that 
+                                         # currently gives no answer.
+                                         # If he approves, this is filled with True.
                  num_of_update_requests = 0, 
                  num_of_meaningful_updates = 0,
+                 #num_of_articles_added_last_update = 0,
                  time_first_created = None, # This field is filled in DBFollow when upoaded
                  time_last_updated = None,  # This field is filled in DBFollow when upoaded
                  time_last_modified_by_user = None,
-
-                 pastResultsKeysList = [], 
+                 total_num_of_articles = 0,       
+                 pastResultsKeysList = None, 
                  url = None):
         
         self.user = user
@@ -65,137 +53,88 @@ class Follow:
         self.num_of_meaningful_updates = num_of_meaningful_updates
         self.time_first_created = time_first_created
         self.time_last_updated = time_last_updated
+        #self.num_of_articles_added_last_update = num_of_articles_added_last_update,
+        self.total_num_of_articles = total_num_of_articles,
         self.time_last_modified_by_user = time_last_modified_by_user
-        
-# Getters and Setters
-# TODO - add all necessary
-
-    def get_pastResultsDict(self):
-        return self.pastResultsDict
-    def get_last_updated(self):
-        return self.last_updated
-    def get_updateFrequency(self):
-        return self.search_params.update_frequency
-    def get_name(self):
-        return self.name
-    def get_url(self):
-        return self.url
-    def get_searchParams(self):
-        return self.search_params
-
-   #Other Methods:
-   # TODO: When tom writes the part that returns details from a key, you can make this function better.
-    def create_email_message(self, diff_list):
-        message = "Hello Dear " + self.username + "!\n"
-        message = message + "This is a new update on your follow named: " + self.follow_name + "\n"
-        message = message + "There are " + str(len(diff_list)) + " new articles: \n\n"
-        for key in diff_list:
-            # TODO: change this back to what it was when TOM changes function
-            # printArticle to __str__
-            message = message + "key: " + key + "\n\n"
-            #message = message + article.printArticle()
-            #message = message + "\t\t****************************************\n\n"
-        message = message + "To remove this follow please press HERE\n\n"
-        message = message + "©  This update brought to you by RESEARCH ASSISTANT\n"
-        return message
-        
-            
-    # update_follow:
-    #   Checks if there are new articles for this follow's url"
-    #   if so: sends mail, updates the date and the saved 
-    def update_follow(self):
-        
-        
-        # OLD DICTIONARY
-        old_dict = self.get_pastResultsDict()
-        if (old_dict == None): 
-            print "update_follow: get_pastResultsDict returned is empty. Quiting..." #TODO: raise exception
-        
-        # NEW DICTIONARY
-        if (self.url == None): 
-            print "update_follow: Couldn't get url for this follow. Quiting..." #TODO: raise exception
-       
-        # TODO: change this part when Tom changes his part.
-        # TODO: this part is only because for now we need to show difference between two URLS
-        if (len(old_dict) != 0):
-            #new_dict = {}
-            new_dict = old_dict.copy()
-            article1 = ArticleData()
-            article1.set_key("new added article")
-            new_dict[article1.get_key()] = article1
-
-        else: 
-            new_dict = GeneralFuncs.url2ArticleDict(self.url)
-            if (new_dict == None): 
-                print "update_follow: Dict returned by url2ArticleDict is empty. Quiting..." #TODO: raise exception
-        my_str = str(new_dict)
-        
-        #diff_dict = GeneralFuncs.compareDicts(old_dict, new_dict)
-        diff_list = GeneralFuncs.compareKeysLists(old_dict.keys(), new_dict.keys())
-        if (len(diff_list) == 0):
-            print "update_follow: No difference From last result was found\n"
+    
+    def create_follow_name_from_search_params(self):
+        if self.search_params == None:
+            return False
+        self.follow_name = ""
+        # if it is citing an article:
+        if (self.search_params.citesArticleName != None and (len(self.search_params.citesArticleName) > 0)):
+            if (len(self.search_params.author) > 0):
+                self.follow_name += "author: " +  self.search_params.author + " + "
+            if (len(self.search_params.keywords) > 0):
+                self.follow_name += "keyword: " +  self.search_params.keywords + " + "
+            self.follow_name += "Documents citing: " 
+            self.follow_name += self.search_params.citesArticleName
         else:
-            # TODO: add here try and catch on the email sending, and only afterwards update follow and add to DB
-            email_message = self.create_email_message(diff_list)
-            GeneralFuncs.sendEmail("lea.stolo@gmail.com", email_message)
-            
-            self.last_updated = datetime.datetime.now()
+            if (len(self.search_params.keywords) != 0):
+                self.follow_name += "keywords: " + self.search_params.keywords
+            if (len(self.search_params.author) != 0):
+                self.follow_name += "author: " + self.search_params.author
+            if (len(self.search_params.journal)!= 0):
+                self.follow_name += "journal : " + self.search_params.journal
+            if (len(self.search_params.year_start) != 0):
+                self.follow_name += "From Year : " + self.search_params.year_start
+        
+        return True
+    
+    # Return value: number of results found 
+    def first_follow_query(self):
+        
+        results = HTMLparser.getAllResultsFromURLwithProxy(self.search_params)
+        #results = resultsObject.get_results() # results is a list of articles
+        self.total_num_of_articles = len(results)
+        
+        self.pastResultsKeysList = []
+        
+        for article in results:
+            self.pastResultsKeysList.append(article.get_key())
+        
+        return len(results)
+        
+    
+    # Before this method, run first_follow_query
+    # this method does not care if query is empty. 
+    # It assumed this was checked before
+    def first_upload(self):
+        
+        if (self.url == None):
+            self.url = self.search_params.constructURL()
+        # convert to DBFollow and insert time fields 
+        db_follow = self.convert2DBFollow()
+        db_follow.time_last_updated = datetime.datetime.now()
+        db_follow.time_last_modified_by_user = datetime.datetime.now()
 
-            # Add the new articles to the saved dictionary
-            for key in diff_list:
-                if self.pastResultsDict.has_key(key):
-                    print "update_follow: There was some problems here.\n The Diff dictionary has a key that already exists in the pastResultsDict\n"       
-                    # TODO - change this print to some exception or something. 
-                else:
-                    self.pastResultsDict[key] = ""
-                    #self.pastResultsDict[key] = diff_dict[key]
-
-        print "Done Follow Updating."
-
-#    def upload_follow_to_DB(self):
-
+        #try: 
+        db_follow.put()
+        #except Exception:
+        #   print "In first upload: Could not upload to DB\n"
+        return True
+        
+   
+# TODO: make everything here in try and catch, so if convert fails, it will write something instead in the field.
+# In case it is a critical value - inform...
     def convert2DBFollow(self):
+        
         db_follow = DBFollow()
-        #db_follow.user = self.username
+        db_follow.user = self.user
+        db_follow.user_nickname = self.user_nickname
         db_follow.follow_name = self.follow_name
-        db_follow.search_params_str = str(self.search_params)  # db.TextProperty()
-        # TODO: check if self.update_frequency is indeed one of the options
+        #db_follow.search_params_str = db.Text(marshal.dumps(self.search_params))  # db.TextProperty()
+        
+        db_follow.search_params_str = pickle.dumps(self.search_params)
         db_follow.update_frequency = self.update_frequency #db.StringProperty(choices=set(["daily","weekly","monthly"]))
-        db_follow.max_results_in_update = self.max_results_in_update #db.IntegerProperty(choices=set([10,20,50]))
-        
-        db_follow.num_of_updates = self.num_of_updates #db.IntegerProperty()
-        db_follow.num_of_successful_updates = self.num_of_successful_updates #db.IntegerProperty()
-        
-        # TODO: Resolve this
-        #db_follow.time_first_created = db.DateTimeProperty()
-        #db_follow.time_last_updated = db.DateTimeProperty()
-        db_follow.pastResultsKeysList_str = str(self.pastResultsKeysList) #db.TextProperty()
-        db_follow.url = self.get_url() #db.TextProperty()
+        db_follow.num_of_update_requests = self.num_of_update_requests #db.IntegerProperty()
+        db_follow.num_of_meaningful_updates = self.num_of_meaningful_updates #db.IntegerProperty()
+        db_follow.time_last_updated = self.time_last_updated
+        db_follow.time_last_modified_by_user = self.time_last_modified_by_user
+        db_follow.pastResultsKeysList = self.pastResultsKeysList # StringListProperty()
+        #db_follow.num_of_articles_added_last_update = self.num_of_articles_added_last_update
+        db_follow.total_num_of_articles = self.total_num_of_articles
+        db_follow.url = self.url #db.TextProperty()
+        #db_follow.time_first_created - is filled automatically
         
         return db_follow
-    
-#    def convert_DBFollow2Follow(db_fllow):
-#        
-#        new_follow = Follow()
-#        new_follow.username = db_fllow.user.nickname()
-#        new_follow.follow_name = db_fllow.follow_name
-#        
-#        new_follow.search_params = eval(db_fllow.search_params_str)  # db.TextProperty()
-#        # TODO: check if self.update_frequency is indeed one of the options
-#        new_follow.update_frequency = db_fllow.update_frequency #db.StringProperty(choices=set(["daily","weekly","monthly"]))
-#        new_follow.max_results_in_update = db_fllow.max_results_in_update #db.IntegerProperty(choices=set([10,20,50]))
-#        
-#        new_follow.num_of_updates = db_fllow.num_of_updates #db.IntegerProperty()
-#        new_follow.num_of_successful_updates = db_fllow.num_of_successful_updates #db.IntegerProperty()
-#        
-#        # TODO: Resolve this
-#        #db_follow.time_first_created = db.DateTimeProperty()
-#        #db_follow.time_last_updated = db.DateTimeProperty()
-#        new_follow.pastResultsKeysList_str = str(db_fllow.pastResultsKeysList) #db.TextProperty()
-#        new_follow.url = db_fllow.get_url() #db.TextProperty()
-#        
-    
-        
-
-
-
