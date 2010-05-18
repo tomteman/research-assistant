2261341c3157798ddea044071e3fb5ed9414914b
lea.stolo@gmail.com <lea.stolo@gmail.com@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-05-18 20:56:42 +0000

    added the creation of follow, and follow.first_follow_query() follow.create_follow_name_from_search_params() follow.first_upload()
    Lea

--- src/FollowFormDone.py
+++ src/FollowFormDone.py
@@ -7,7 +7,7 @@ from google.appengine.ext import webapp
 from django.template import Template,Context
 from django.conf import settings 
 from django.template.loader import get_template
-
+from google.appengine.ext import db
 
 from SearchParams import *
 
@@ -20,7 +20,7 @@ class FollowFormDone(webapp.RequestHandler):
         # = self.request.get()
         forms = ("follow_name", "ch_keywords", "keywords",
                   "ch_author", "author", "ch_journal", "journal",
-                 "ch_citing", "citing_num", "update_frequency" )
+                 "ch_citing", "citing_num", "update_frequency" ,"article")
         
        
         vars = {}
@@ -29,12 +29,16 @@ class FollowFormDone(webapp.RequestHandler):
            
         keywords = vars["keywords"] if vars['ch_keywords'] else ""
         author = vars["author"] if vars["ch_author"] else ""              
-        journal = vars["journal"] if vars["ch_journal"] else ""
-        
-        cites =  vars["citing_num"] if vars["ch_citing"] else ""
+        journal = vars["journal"] if vars["ch_journal"] else ""      
+        citationsID =  vars["citing_num"] if vars["ch_citing"] else ""
+        citesTitle =  vars["article"] if vars["ch_citing"] else ""
                        
-        s_params = SearchParams(keywords = keywords,  author=author, journal=journal,
-                                num_of_results = 100, cites = cites )
+        s_params = SearchParams(keywords = keywords,  
+                                author=author, 
+                                journal=journal,
+                                num_of_results = 100, 
+                                citationsID = citationsID, 
+                                citesArticleName = citesTitle)
  
         user = users.get_current_user()
         if (user != None):
@@ -44,15 +48,28 @@ class FollowFormDone(webapp.RequestHandler):
             user_nickname = None
             user_id = None
       
-        follow = Follow.Follow(user = user, user_nickname=user_nickname, user_id=user_id, follow_name=vars["follow_name"],
-                               search_params=s_params, update_frequency=vars["update_frequency"], url = s_params)
-            
-           
-        try: 
-            follow.first_upload()
-        except:
-            #ResearchExceptions.InputError: #as detail:
-            pass   
+        follow = Follow.Follow(user = user, 
+                               user_nickname=user_nickname, 
+                               user_id=user_id, 
+                               follow_name=vars["follow_name"],
+                               search_params=s_params, 
+                               update_frequency=vars["update_frequency"])
+                               
+        if (follow.first_follow_query() == 0): # no results where found
+            # TODO: need to notify user there are no results for this query
+            pass
+        follow.create_follow_name_from_search_params()
+        follow.first_upload()
+        
+#        #dbfollow = db.get("ahJyZXNlYXJjaC1hc3Npc3RhbnRyDwsSCERCRm9sbG93GI8CDA")
+#        query = db.GqlQuery("SELECT * FROM DBFollow WHERE follow_name = 'keyword: instantand Documents citing: Instant messaging in teen life'")
+#        dbfollow = query.fetch(1)[0]
+#        dbfollow.update_DBfollow()
+#        
+#        query = db.GqlQuery("SELECT * FROM DBFollow WHERE follow_name = 'keyword: interesting + Documents citing: Syskill \& Webert: Identifying interesting web sites'")
+#        dbfollow = query.fetch(1)[0]
+#        dbfollow.update_DBfollow()
+        
                         
 
         t = get_template('followFormDone.html')
