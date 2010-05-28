3108f291d3cf35443958850ef47053789f242289
maverick42 <maverick42@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-05-28 11:39:22 +0000

    changed proxy to 211.138.124.198:80

--- src/HTMLparser.py
+++ src/HTMLparser.py
@@ -473,7 +473,7 @@ def getResultsFromURLwithProxy(url):
     # first we fetch the HTML from Google Scholar
     newHTML = getHTML(url)
     #newHTML.getHTMLfromURLwithProxy("74.115.1.13", 80)
-    newHTML.getHTMLfromURLwithProxy("221.130.13.228", 80)
+    newHTML.getHTMLfromURLwithProxy("211.138.124.198", 80)
     
     # parse the results
     newHTMLdata = HTMLparser(url,newHTML.get_html())
