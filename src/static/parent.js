eea712ed88bf578d4241ed7cc35501e983991963
dina.vain <dina.vain@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-06-04 17:04:40 +0000

    Added: 	labels display on the sidebar

--- src/static/parent.js
+++ src/static/parent.js
@@ -3,6 +3,71 @@ var labels
 var uniqueLabels   
 /* list of unique label names - only names*/
 var uniqueLabelsNames = []
+                
+var menuStatus = 0;                         
+                         
+$(function(){
+	
+	$(".labelButton")
+	.button()
+	.click( function() {alert( "Running the last action" );})
+	.next()
+		.button( {
+			text: false,
+			icons: {
+			primary: "ui-icon-triangle-1-s"
+		}}).click( function() {menu($(this).parent());})
+	.parent()
+		.buttonset();
+	
+	
+      getLabelsFromDB();
+      getUniqueLabelsFromDB();
+      
+    	  
+ });
+
+/////////////////////////////////////////////////////////////////////////
+function printLabels(){
+	$.each(uniqueLabelsNames,function(intIndex, objValue){
+		alert(objValue);
+		addLabel(objValue);
+	});
+	
+}
+
+                        
+function addLabel(labelName, number){
+	var newLabel = $("#firstLabel").clone();
+	var labelButton = newLabel.find(".labelButton");
+	newLabel.show();
+	labelButton.val(labelName + "("+number+")");
+	labelButton.attr("id", labelName);
+	labelButton.click( function() { showLabeledArticles(labelButton.attr("id")) } );
+	newLabel.find("#menuButton").click( function() {menu($(this).parent());})
+	
+	$("#labelList").append(newLabel);
+	
+}
+
+
+function showLabeledArticles(label_name){
+	$("#the_iframe").attr("src", '/ShowArticlesByLabel?Id='+label_name);
+	
+}
+
+function menu(parent){
+	if (menuStatus == 0){
+		parent.parent().find("#dropDown").show();
+		menuStatus=1;
+	}else{
+		parent.parent().find("#dropDown").hide();
+		menuStatus=0;
+	}	
+}
+
+
+                         
 
 function getLabelsFromDB(){
 	$.getJSON('/GetAllLabels?Type=All',function(data){
@@ -17,22 +82,21 @@ function getUniqueLabelsFromDB(){
 	});
 }
 
+
 function getUniqueLabelsNamesList(){
 	$.each(uniqueLabels,function(intIndex, objValue){
 		uniqueLabelsNames.push(objValue.label_name)
+		addLabel(objValue.label_name, objValue.number);
 	});
 }
 
 function extractUniqueLabelList(){
 	$.each(labels, function(intIndex , objValue){
 		if (($.inArray(objValue.label_name, uniqueLabels)) == -1)
-			uniqueLabels.push(objValue.label_name)	
+			uniqueLabels.push(objValue.label_name)
+			
 		});
 }
 
-$(function(){
-	getLabelsFromDB();
-	getUniqueLabelsFromDB();
-});
 
 
