ccc919ca28585254d24388b3b5647d2c915fd58e
Lea.stolo@gmail.com <Lea.stolo@gmail.com@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-06-05 13:27:38 +0000

    added pending_share_preview_as_HTMLparser

--- src/PendingSharedLabel.py
+++ src/PendingSharedLabel.py
@@ -2,6 +2,8 @@ from google.appengine.ext import db
 from google.appengine.api import users 
 from google.appengine.ext import webapp
 import Label
+import HTMLparser
+
 
 class PendingSharedLabel(db.Model):
         inviting_user = db.UserProperty()
@@ -10,6 +12,12 @@ class PendingSharedLabel(db.Model):
         time_mail_sent = db.DateTimeProperty(auto_now_add=True)
         Id = db.StringProperty() # this will actually be the key to the DB
         
+
+# RC = -2 == email address not valid
+# RC = -3 == new_user already has this label
+# RC = -4 == no results where found  
+# RC = -5 == too many results
+# RC = -7 == no connection to DB
         
 def get_single_pending(invited_user, pending_id):
     query = db.GqlQuery("SELECT * FROM PendingSharedLabel WHERE invited_user = :1 "+
@@ -18,9 +26,9 @@ def get_single_pending(invited_user, pending_id):
     
     num_results = query.count(10)
     if (num_results > 1):
-        return -4 
+        return -5 
     if (num_results == 0):
-        return -5
+        return -4
     return query.fetch(2)[0]
 
 def get_all_users_PendingSharedLabel(user):
@@ -57,4 +65,22 @@ def acceptPendingSharedLabel(invited_user, pending_id):
             return rc
         pending_obj.delete()
     return True
+
+# RC = -4 == no results where found  
+# RC = -5 == too many results
+# RC = -6 == inviter_user has no label with the name label_name
+def pending_share_preview_as_HTMLparser(invited_user, pending_id):
+    pending_obj = get_single_pending(invited_user, pending_id)
+    if not isinstance(pending_obj, PendingSharedLabel):
+        return pending_obj
+    
+    article_objects_list = Label.get_articles_list_with_label(pending_obj.inviting_user, pending_obj.label_name)
+    if (article_objects_list == -4):
+        return -6
+    
+    html_parser = HTMLparser.HTMLparser(url=None, html=None)
+    html_parser.results =  article_objects_list
+    html_parser.numOfResults = len(article_objects_list)
+    return html_parser
+    
     
