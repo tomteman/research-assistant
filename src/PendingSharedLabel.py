d1195db161dc12165fbfe39e1f14b6bc66928236
Lea.stolo@gmail.com <Lea.stolo@gmail.com@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-06-05 15:54:16 +0000

    chnged return values in success to 0 instead of True, and return -7 if DB is off.

--- src/PendingSharedLabel.py
+++ src/PendingSharedLabel.py
@@ -32,30 +32,39 @@ def get_single_pending(invited_user, pending_id):
     return query.fetch(2)[0]
 
 def get_all_users_PendingSharedLabel(user):
-    query = db.GqlQuery("SELECT * FROM PendingSharedLabel WHERE invited_user = :1 ",user) 
-    pending_list = []   
-    for pending_label in query:
-        pending_list.append(pending_label)
-    return pending_list
+    try:
+        query = db.GqlQuery("SELECT * FROM PendingSharedLabel WHERE invited_user = :1 ",user) 
+        pending_list = []   
+        for pending_label in query:
+            pending_list.append(pending_label)
+        return pending_list
+    except Exception:
+        return -7
 
 def remove_PendingSharedLabel(invited_user, pending_id):
-    query = db.GqlQuery("SELECT * FROM PendingSharedLabel WHERE invited_user = :1 " + 
-                        "AND Id = :2",
-                        invited_user, pending_id)
-    count = 0
-    for dbfollow in query:
-        dbfollow.delete()
-        count += 1
-    return count
+    try:
+        query = db.GqlQuery("SELECT * FROM PendingSharedLabel WHERE invited_user = :1 " + 
+                            "AND Id = :2",
+                            invited_user, pending_id)
+        count = 0
+        for dbfollow in query:
+            dbfollow.delete()
+            count += 1
+        return 0
+    except Exception:
+        return -7
 
 
     
 # RC = -4 == no results where found for label_name and user
 def acceptPendingSharedLabel(invited_user, pending_id):
-    query = db.GqlQuery("SELECT * FROM PendingSharedLabel WHERE invited_user = :1 "+ 
-                    "AND Id = :2 ", 
-                    invited_user, pending_id)
-    
+    try:
+        query = db.GqlQuery("SELECT * FROM PendingSharedLabel WHERE invited_user = :1 "+ 
+                        "AND Id = :2 ", 
+                        invited_user, pending_id)
+    except Exception:
+        return -7   
+     
     num_results = query.count(10)
     if (num_results == 0):
         return -4
@@ -63,9 +72,14 @@ def acceptPendingSharedLabel(invited_user, pending_id):
         rc = Label.execute_label_sharing_after_approved(pending_obj.inviting_user,pending_obj.label_name,pending_obj.invited_user)
         if (rc != True):
             return rc
-        pending_obj.delete()
-    return True
-
+        try:
+            pending_obj.delete()
+        except Exception:
+            return -7
+        
+    return 0
+    
+# RC = -3 == new_user already has this label
 # RC = -4 == no results where found  
 # RC = -5 == too many results
 # RC = -6 == inviter_user has no label with the name label_name
@@ -75,6 +89,8 @@ def pending_share_preview_as_HTMLparser(invited_user, pending_id):
         return pending_obj
     
     article_objects_list = Label.get_articles_list_with_label(pending_obj.inviting_user, pending_obj.label_name)
+    if (article_objects_list == -3):
+        return -3
     if (article_objects_list == -4):
         return -6
     
