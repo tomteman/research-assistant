6d8d0268d03512f34396e6cf6effeda36c24921f
Lea.stolo@gmail.com <Lea.stolo@gmail.com@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-08-19 12:22:48 +0000

    added method: send_first_status_email_by_request

--- src/Follow.py
+++ src/Follow.py
@@ -1,4 +1,5 @@
 from SearchParams import *
+from google.appengine.api import mail
 from ArticleData import ArticleData
 import datetime
 import string
@@ -27,6 +28,7 @@ class Follow:
                  time_last_updated = None,  # This field is filled in DBFollow when upoaded
                  total_num_of_articles = 0,       
                  pastResultsKeysList = None, 
+                 first_results = None, # results object from first run when first created. I use this only for the first email, if requested
                  url = None):
         
         self.user = user
@@ -82,6 +84,7 @@ class Follow:
     def first_follow_query(self):
         
         results = HTMLparser.getAllResultsFromURLwithProxy(self.search_params)
+        self.first_results = results
         #results = resultsObject.get_results() # results is a list of articles
         self.total_num_of_articles = len(results)
         
@@ -111,7 +114,63 @@ class Follow:
        
         return True
         
-   
+    # In the Follow created page, we added a button so you can send a mail.
+    # whwen pressing this button it sends an email with all current articles 
+    # (up to 1000 as usual, since this is Google Scholar limitation)
+    def send_first_status_email_by_request(self):
+        
+        plain_msg = "Your new Follow named " + self.follow_name + " has been created.\n"
+        plain_msg = plain_msg +  "Current articles matching you search terms are listed bellow: <br>"
+
+        html_msg = "<html><body>"
+        html_msg = html_msg + "<b>Your new Follow named " + self.follow_name + " has been created. </b><br>"
+        html_msg = html_msg + "Current articles matching you search terms are listed bellow: <br>"
+        
+        # create dictionary of new articles to report
+        tmp_dict = {}
+        for article in self.first_results:
+                plain_msg = plain_msg + "\n" + unicode(article.get_article_title(), errors='ignore') + "\n\n"
+                
+                if (len(article.get_article_url()) > 0):
+                    html_msg = html_msg + "<a href =\"" + article.get_article_url() +""""<font color="6633cc">""" + unicode(article.get_article_title(), errors='ignore') + "</font></a><br>"
+                else: 
+                    html_msg = html_msg + """<b><font color="#6633cc">""" + unicode(article.get_article_title(), errors='ignore') + "</b></font><br>"
+                html_msg = html_msg + """<font color="#00cc66">""" + unicode(article.get_HTML_author_year_pub(), errors='ignore') + "</font>"
+                html_msg = html_msg + unicode(article.get_HTML_abstract(), errors='ignore') + "<br>"
+                html_msg = html_msg + """<hr size="3" width="100%" align="left" color="009999"></hr><br>"""
+        
+        
+        html_msg = html_msg + """<a href =\"http://research-assistant.appspot.com/?page=MyFollows\" <font color=\"6633cc\"> List My Follows</font></a><br><br>"""
+        html_msg = html_msg + "&copy; This update brought to you by <a href=http://research-assistant.appspot.com/> Research Assistant</a><br>"
+        html_msg = html_msg + "</body></html>"
+        short_name = str(self.follow_name)[:40]
+        try: 
+            if (len(self.follow_name) > 40): 
+                mail.send_mail(sender="Research Assistant Team <tau.research.assistant@gmail.com>",
+                              to=self.user.email(),
+                              subject="Your Follow: " + short_name +  "... was created", 
+                              body=plain_msg, 
+                              html=html_msg)
+            else: # no "..."
+                 mail.send_mail(sender="Research Assistant Team <tau.research.assistant@gmail.com>",
+                          to=self.user.email(),
+                          subject="Your Follow: " + short_name +  " was created", 
+                          body=plain_msg, 
+                          html=html_msg)
+                
+        except Exception:
+            return False
+        ## Sending another mail specifically to Lea.
+        mail.send_mail(sender="Research Assistant Team <tau.research.assistant@gmail.com>",
+                      to="lea.stolo@gmail.com",
+                      subject="Your Follow: " + short_name +  "... was created", 
+                      body=plain_msg, 
+                      html=html_msg)
+        
+        #print plain_msg
+        return True
+    
+        
     def convert2DBFollow(self):
         
         db_follow = DBFollow()
