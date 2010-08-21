a16573c8e48b8b8c4b3f59252975ee7d79c88a9d
Lea.stolo@gmail.com <Lea.stolo@gmail.com@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-08-21 14:33:09 +0000

    added some fields to RA_User

--- src/RA_User.py
+++ src/RA_User.py
@@ -5,24 +5,24 @@ from google.appengine.ext import webapp
 
 class RA_User(db.Model):
         user = db.UserProperty()
-        label_name_to_local_name = db.StringProperty() #serialized dictionary
-        local_name_to_label_name = db.StringProperty() #serialized dictionary (opposite to label_name_to_local_name) 
         friends_emails = db.StringListProperty()
+        last_time_updated_suggestions = db.DateTimeProperty()
+        list_of_labeled_article_keys = db.StringListProperty()
+        list_of_suggested_article_keys = db.StringListProperty()
 
-# RC = -2 == email address not valid
-# RC = -3 == new_user already has this label
-# RC = -4 == no results where found  
-# RC = -5 == too many results
-# RC = -7 == no connection to DB
+
 def get_RA_User_obj(user):
-    query = db.GqlQuery("SELECT * FROM RA_User WHERE user = :1 ",user)
-    num_res = query.count(10)
-    if (num_res == 0):
-        return -4
-    elif (num_res > 1):
-        return -5
-    else:
-        return query.fetch(1)[0]
+    try:
+        query = db.GqlQuery("SELECT * FROM RA_User WHERE user = :1 ", user)
+            
+        if (query.count(2) == 0):
+            return -7
+            
+        ra_user_obj = query.fetch(2)[0]
+        return ra_user_obj
+    except Exception:
+        return -7
+
 
 def get_friends_emails_list(user):
     return (get_RA_User_obj(user)).friends_emails
