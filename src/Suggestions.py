ba5f4c1db357e56610df7f55f9d6b31f4ff4a5b1
Lea.stolo@gmail.com <Lea.stolo@gmail.com@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-08-26 18:18:11 +0000

    splited to get_list_of_suggested_article_ordered_by_rank get_list_of_suggested_article_ordered_by_date

--- src/Suggestions.py
+++ src/Suggestions.py
@@ -102,7 +102,7 @@ class Suggestion_mechanism:
             self.relevant_users_list.remove(self.main_user)
         
         
-        for users_subset_size in range(2, self.MAX_NUM_OF_USER_SUBSET):
+        for users_subset_size in reversed(range(2, self.MAX_NUM_OF_USER_SUBSET)):
             
             for subset_of_users in combinations(self.relevant_users_list, users_subset_size):
 
@@ -208,7 +208,7 @@ def sort_list_of_suggestions_by_date(list_of_suggestions):
 # created last week first -ordered within by rank
 # after that all the rest - ordered by rank
 # igonre suggestions that where already deleted 
-def get_list_of_suggested_article_ordered(user_name):
+def get_list_of_suggested_article_ordered_by_rank(user_name):
     suggestions_list = []
     final_articles_list = []
     query = db.GqlQuery("SELECT * FROM Suggestion WHERE user = :1", user_name)
@@ -218,7 +218,6 @@ def get_list_of_suggested_article_ordered(user_name):
         if (not sugg.is_removed):
             suggestions_list.append(sugg)    
     
-    
     for sugg in sort_list_of_suggestions_by_rank(suggestions_list):
         final_articles_list.append(pickle.loads(str(sugg.suggested_serialized_article)))
     
@@ -227,8 +226,43 @@ def get_list_of_suggested_article_ordered(user_name):
     html_parser.numOfResults = len(final_articles_list)
     return html_parser
 
+# return the articles in the following order:
+# created last week first -ordered within by rank
+# after that all the rest - ordered by rank
+# igonre suggestions that where already deleted 
+def get_list_of_suggested_article_ordered_by_date(user_name):
+    suggestions_list = []
+    final_articles_list = []
+    query = db.GqlQuery("SELECT * FROM Suggestion WHERE user = :1", user_name)
+        # results = q.fetch(10)
+        # this is supposed to be only one result but who knows...
+    for sugg in query:
+        if (not sugg.is_removed):
+            suggestions_list.append(sugg)    
+    
+    for sugg in sort_list_of_suggestions_by_date(suggestions_list):
+        final_articles_list.append(pickle.loads(str(sugg.suggested_serialized_article)))
+    
+    html_parser = HTMLparser.HTMLparser(url=None, html=None)
+    html_parser.results = final_articles_list
+    html_parser.numOfResults = len(final_articles_list)
+    return html_parser
+
+
 def get_num_of_suggestions_for_user(user_name):
     query = db.GqlQuery("SELECT * FROM Suggestion WHERE user = :1", user_name)
     return query.count()
-    
 
+# this function turns the is_removed to True but leaves the suggestion
+# in the DB so that the user wont get the same suggestion again
+def remove_suggestion(user_name, article_key):
+    query = db.GqlQuery("SELECT * FROM Suggestion WHERE user = :1" + 
+                        "AND suggested_article_key = :2", user_name, article_key)
+    if (query.count(1) == 0):
+        return -4
+    
+    suggestion = query.fetch(2)[0]
+    suggestion.is_removed = True
+    suggestion.put()
+    
+    
