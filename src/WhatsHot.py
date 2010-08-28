ba87e3d1ce8052fe9120fe5ea7c41ddfc9857666
romalabunsky <romalabunsky@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-08-28 15:31:53 +0000

    handle no suggestions scenario

--- src/WhatsHot.py
+++ src/WhatsHot.py
@@ -12,6 +12,8 @@ import PendingSharedLabel
 from Suggestions import get_list_of_suggested_article_ordered_by_rank
 from Suggestions import get_list_of_suggested_article_ordered_by_date
 from Suggestions import remove_suggestion
+from Suggestions import get_num_of_suggestions_for_user
+
 import Label
 import JSONConvertors
 import HTMLparser
@@ -33,18 +35,25 @@ class removeSuggestedFromDB(webapp.RequestHandler):
 class ShowHot(webapp.RequestHandler):
 
     def get(self):
-        t = get_template('search.html')
+        
         c = Context()
         user = users.get_current_user()
         
         htmlParser = get_list_of_suggested_article_ordered_by_rank(user)
-        results = htmlParser.results
-        my_html_parser_encoder = JSONConvertors.HTMLparserEncoder()
-        resultsJSON = my_html_parser_encoder.encode(htmlParser)
-        c['suggestFlag'] = 1
-        c['users'] = users
-        c['results'] = results
-        c['resultsJSON'] = resultsJSON
-        c['formAction'] = '/AddFollow'
-        c['showlabel'] = True
+        numOfSuggestions = get_num_of_suggestions_for_user
+        if(numOfSuggestions == 0):
+            t = get_template('noResults.html')
+            c['noSuggestions'] = 1
+
+        else:
+            t = get_template('search.html')
+            results = htmlParser.results
+            my_html_parser_encoder = JSONConvertors.HTMLparserEncoder()
+            resultsJSON = my_html_parser_encoder.encode(htmlParser)
+            c['suggestFlag'] = 1
+            c['users'] = users
+            c['results'] = results
+            c['resultsJSON'] = resultsJSON
+            c['formAction'] = '/AddFollow'
+            c['showlabel'] = True
         self.response.out.write(t.render(c))
