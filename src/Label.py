b221004cca62f149c1926bb3d4cf607f0c3ccf7a
Lea.stolo@gmail.com <Lea.stolo@gmail.com@ca89b361-1582-40f2-70bb-04278c7f517c>
2010-08-30 13:49:29 +0000

    finished get_label_by_email - working!

--- src/Label.py
+++ src/Label.py
@@ -351,15 +351,6 @@ def share_label_request(inviting_user, label_name, invited_user_email, notify=Tr
     if not mail.is_email_valid(invited_user_email):
         return -2
     invited_user = users.User(invited_user_email)
-    
-    # append emails to lists of friends
-#    inviting_user_ra_obj = RA_User.get_RA_User_obj(inviting_user)
-#    invited_user_ra_obj = RA_User.get_RA_User_obj(invited_user)
-#    if (invited_user.email() not in inviting_user_ra_obj.friends_emails):
-#        inviting_user_ra_obj.friends_emails.append(invited_user.email())
-#    if (inviting_user.email() not in invited_user_ra_obj.friends_emails):
-#        invited_user_ra_obj.friends_emails.append(inviting_user.email())
-#    
     # check if this label really exists at the inviting user
     query_inviting = db.GqlQuery("SELECT * FROM Label WHERE users_list = :1 "+
                     "AND label_name = :2 ", 
@@ -555,7 +546,7 @@ def search_in_labels_return_HTMLparser(user, label_name, search_term):
     return html_parser
 
 def search_in_labels_return_HTMLparser_JSON(user, label_name, search_term):
-    
+    plain_msg = ""
     query = db.GqlQuery("SELECT * FROM Label WHERE users_list = :1 "+
                 "AND label_name = :2",  
                 user, label_name)
@@ -576,67 +567,53 @@ def search_in_labels_return_HTMLparser_JSON(user, label_name, search_term):
 
 
 def get_label_by_email(sending_user, target_user_email, label_name):
-    mail.send_mail(sender="Research Assistant Team <tau.research.assistant@gmail.com>",
-                  to=target_user_email,
-                  subject= "Articles labeled by",   
-                  body="", 
-                  html="<html><body>LeaLea</body></html>")
+    new_user = users.User(target_user_email)
+    plain_msg = " "
+    try: 
+        ## GET ALL ARTICLED
+        query = db.GqlQuery("SELECT * FROM Label WHERE users_list = :1 "+
+                        "AND label_name = :2 ", 
+                        sending_user, label_name)
+        article_objects_list = []
+        for label_object in query:
+            article_objects_list.append(pickle.loads(str(label_object.serialized_article)))
+    
+        # PREFIX
+        html_msg = "<html><body>"
+        html_msg = html_msg + "<b>Hello Dear " + str(target_user_email) + ",</b><br>"
+        html_msg = html_msg + "Following are all articles labeled by " + sending_user.nickname() + " as "
+        html_msg = html_msg + "<font color=\"red\" ><b>" + str(label_name) +  "</b></font></br><br><br><br>"
+        
+        # JUST FOR NEW USERS
+        is_new_user = is_new_user_to_RA(new_user)
+        if (is_new_user):
+            html_msg += get_html_message_for_new_user()
+        
+        # BUILD CONTENT
+        for article_obj in article_objects_list:
+            if (len(article_obj.get_article_url()) > 0):
+                html_msg = html_msg + "<a href =\"" + article_obj.get_article_url() +""""<font color="6633cc">""" + article_obj.get_article_title() + "</font></a><br>"
+            else: 
+                html_msg = html_msg + """<b><font color="#6633cc">""" + article_obj.get_article_title() + "</b></font><br>"
+            html_msg = html_msg + """<font color="#00cc66">""" + article_obj.get_HTML_author_year_pub() + "</font>"
+            html_msg = html_msg + article_obj.get_HTML_abstract() + "<br>"
+            html_msg = html_msg + """<hr size="3" width="100%" align="left" color="009999"></hr><br>"""
+    
+        html_msg = html_msg + "<br>&copy; brought to you by <a href=http://research-assistant.appspot.com/> Research Assistant</a><br>"
+        html_msg = html_msg + "</body></html>"
+        
+        # SEND MESSAGE
+        mail.send_mail(sender="Research Assistant Team <tau.research.assistant@gmail.com>",
+                  to=new_user.email(),
+                  subject="Articles labeled by " + sending_user.nickname() + " as " + label_name,
+                  body=plain_msg, 
+                  html=html_msg)
+    except Exception: 
+        msg = "bla"
+        msg = str(sys.exc_info()[0]) + str(sys.exc_info()[1]) + str(sys.exc_info()[2])
+        return msg
+    
     return True
-       
-#    ## GET ALL ARTICLED
-#    try:
-#        query = db.GqlQuery("SELECT * FROM Label WHERE users_list = :1 "+
-#                        "AND label_name = :2 ", 
-#                        sending_user, label_name)
-#        article_objects_list = []
-#        for label_object in query:
-#            article_objects_list.append(pickle.loads(str(label_object.serialized_article)))
-#    except Exception:
-#        msg = "bla"
-#        msg = str(sys.exc_info()[0]) + str(sys.exc_info()[1]) + str(sys.exc_info()[2])
-#        return msg
-#    
-#    
-#    ## PREFIX
-#    plain_msg = ""
-#    html_msg = "<html><body>"
-#    html_msg = html_msg + "<b>Hello Dear " + str(target_user_email) + ",</b><br><br>"
-#    html_msg = html_msg + "Following are all articles labeled by " + sending_user.nickname() + " as "
-#    html_msg = html_msg + "<font color=\"red\" ><b>" + str(label_name) +  "</b></font></br><br>"
-#    
-#    ## JUST FOR NEW USERS
-#    new_user = users.User(target_user_email)
-#    is_new_user = is_new_user_to_RA(new_user)
-#    if (is_new_user):
-#        html_msg += get_html_message_for_new_user()
-#    
-#    ## BUILD CONTENT
-#    for article_obj in article_objects_list:
-#        if (len(article_obj.get_article_url()) > 0):
-#            html_msg = html_msg + "<a href =\"" + article_obj.get_article_url() +""""<font color="6633cc">""" + unicode(article_obj.get_article_title(), errors='ignore') + "</font></a><br>"
-#        else: 
-#            html_msg = html_msg + """<b><font color="#6633cc">""" + unicode(article_obj.get_article_title(), errors='ignore') + "</b></font><br>"
-#        html_msg = html_msg + """<font color="#00cc66">""" + unicode(article_obj.get_HTML_author_year_pub(), errors='ignore') + "</font>"
-#        html_msg = html_msg + unicode(article_obj.get_HTML_abstract(), errors='ignore') + "<br>"
-#        html_msg = html_msg + """<hr size="3" width="100%" align="left" color="009999"></hr><br>"""
-#
-#
-#    html_msg = html_msg + """<br><hr size="3" width="100%" align="left" color="009999"></hr><br>"""
-#    html_msg = html_msg + "<br>&copy; brought to you by <a href=http://research-assistant.appspot.com/> Research Assistant</a><br>"
-#    html_msg = html_msg + "</body></html>"
-#    
-#    ## SEND MESSAGE
-#    try: 
-#        mail.send_mail(sender="Research Assistant Team <tau.research.assistant@gmail.com>",
-#                      to=target_user_email,
-#                      subject= "Articles labeled by " + str(sending_user.email()) + " as " + str(label_name), 
-#                      body=plain_msg, 
-#                      html=html_msg)
-#    except Exception:
-#        msg = "bla"
-#        msg = str(sys.exc_info()[0]) + str(sys.exc_info()[1]) + str(sys.exc_info()[2])
-#        return msg
-#    return True
 
 
 def get_html_message_for_new_user():
